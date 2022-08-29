874beffb0d9fc4878030ce56b5f4395fce8bb46b
Jochyd <mail>
2022-08-29 16:48:51 -0300

    agregamos una ventana de mensaje para preguntar si el usuario esta seguro de eliminar la pelicula

--- js/admin.js
+++ js/admin.js
@@ -35,7 +35,7 @@ function cargaInicial() {
 function crearFila(itemPelicula) {
   //creando la tabla
   let tablaPeliculas = document.getElementById("tablaPeliculas");
-  tablaPeliculas.innerHTML +=`<tr>
+  tablaPeliculas.innerHTML += `<tr>
    <th scope="row">${itemPelicula.codigo}</th>
    <td>${itemPelicula.titulo}</td>
    <td>
@@ -48,15 +48,16 @@ function crearFila(itemPelicula) {
      ${itemPelicula.genero}
    </td>
    <td>
-     <button type="button" class="btn btn-danger">
-       <i class="bi bi-file-x-fill colorIncono fs-5"></i>
-     </button>
-     <button type="button" class="btn btn-warning mt-2">
-       <i class="bi bi-pencil-square fs-5"></i>
-     </button>
-   </td>
+   <button type="button" class="btn btn-danger" onclick="borrarPelicula('${itemPelicula.codigo}')">
+     <i class="bi bi-file-x-fill colorIncono fs-5"></i>
+   </button>
+   <button type="button" class="btn btn-warning mt-2">
+     <i class="bi bi-pencil-square fs-5"></i>
+   </button>
+ </td>
  </tr>`;
 }
+
 
 function mostrarFormulario() {
   modalPelicula.show();
@@ -104,3 +105,49 @@ function guardarPeliculasEnLocalStorage() {
   //setitem para guardar el archivo.. se pone una palabra que yo invento para acceder al local 2do parametro el value que son los datos que voy a guaradar en formato json son el arreglo, para transformarlo en formato json se pone (string numeber, objetos, arreglos, un texto, etc, menos los undifined) se pone JSON.stringify(EL ARREGLO)
   localStorage.setItem("listaPeliculasKey", JSON.stringify(listaPeliculas));
 }
+function borrarTabla(){
+  let tablaPeliculas = document.getElementById("tablaPeliculas");
+  tablaPeliculas.innerHTML = "";
+}
+cargarInicial();
+//muestre la tabla con datos
+function cargarInicial(){
+  if(listaPeliculas.length >0){
+    //dibujar filas de la tabla
+    listaPeliculas.forEach((itemPelicula)=>{crearFila(itemPelicula)})
+  }
+}
+//esto es para cuando es de tipo modulo y en el maquetado ponenmos onclick
+//ventaniita
+
+window.borrarPelicula = function (codigo) {
+  console.log(codigo);
+  Swal.fire({
+    title: "Eliminar pelicula",
+    text: "Esta seguro de eliminar la pelicula seleccionada, no se puede revertir este paso",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Borrar",
+    CancelButtonText: "Cancelar",
+  }).then((result) => {
+    console.log(result);
+    if (result.isConfirmed) {
+      //agregar el codigo para borrar la pelicula
+      //buscar la pelicula con el codigo indicado en el arreglo y borrarlo
+      let copiaListaPeliculas = listaPeliculas.filter((pelicula) => {
+        return pelicula.codigo != codigo;
+      });
+      console.log(copiaListaPeliculas);
+      listaPeliculas = copiaListaPeliculas;
+      //actualizar el localstorage
+      guardarPeliculasEnLocalStorage();
+      //actualizar la tabla
+      borrarTabla();
+      cargarInicial();
+
+      Swal.fire("Pelicula!", "La pelicula seleccionada fue eliminada", "success");
+    }
+  });
+};
